2923ca95545068ab64c6f35125c693a4068924ad
mihail <slipper78921@gmail.com>
2025-02-07 16:59:38 +0300

    add new file

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 print("Hello")
+import telebot
+token = "7925900642:AAG5BuVLwIvadstEaGgx_pzdySwnJwPxhoI"
+text = ""
+ling = ""
+clients = []
+admins = [6210349680]
+bot=telebot.TeleBot(token)
+
+
+
+
+
+@bot.message_handler(commands=["start"])
+def start_message(message):
+    if message.chat.id in admins:
+        bot.send_message(message.chat.id, "привет")
+    else:
+        clients.append(message.chat.id)
+bot.infinity_poling()
+
+
+
