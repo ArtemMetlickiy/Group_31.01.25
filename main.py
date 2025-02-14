40f5873911d14e8796eaed9c4efda100796511c3
artem <aasbaa39@gmail.com>
2025-02-14 16:54:22 +0300

    artem

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 import telebot
+from telebot import types
+from artem import artem
 token = "7925900642:AAG5BuVLwIvadstEaGgx_pzdySwnJwPxhoI"
 text = ""
 ling = ""
@@ -17,6 +19,17 @@ def start_message(message):
         bot.send_message(message.chat.id, "привет")
     else:
         clients.append(message.chat.id)
+
+
+@bot.message_handler(commands=["artem"])
+def art(message):
+    artem(message, bot, types)
+
+
+
+
+
+
 bot.infinity_poling()
 
 
